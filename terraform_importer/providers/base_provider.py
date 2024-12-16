da148b63181f747c2d17071fc0aa374354fc4331
eyalya <42469346+eyalya@users.noreply.github.com>
2024-12-16 21:48:42 +0200

    Add bitbucket provider (#1)

--- terraform_importer/providers/base_provider.py
+++ terraform_importer/providers/base_provider.py
@@ -1,28 +1,34 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict, Optional
+import logging
 
+# Define a global logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+global_logger = logging.getLogger("GlobalLogger")
 
 # Abstract Base Class for Providers
 class BaseProvider(ABC):
     """Abstract base class for all providers."""
-    
-    @abstractmethod
-    def get_id(self, resource_type: str, resource_block: dict) -> Optional[str]:
+
+    def __init__(self):
         """
-        Fetches the resource ID for a given resource block.
+        Initializes the Base Provider with required attributes.
+        """
+        self._resources_dict = {}
+    
+    def get_id(self, resource_type: str, resource_block: Dict) -> Optional[str]:
+        """
+        Fetches the ID for a specific resource type and resource block.
         Args:
-            resource_type (str): Type of the resource (e.g., "aws_instance").
-            resource_block (dict): Configuration block for the resource.
+            resource_type (str): The type of the resource (e.g., 'aws_instance').
+            resource_block (Dict): The resource configuration block.
         Returns:
-            Optional[str]: Resource ID if found, else None.
+            Optional[str]: Resource ID if found, or None if not found.
         """
-        pass
+        if hasattr(self, resource_type):
+            method = getattr(self, resource_type)
+            return method(resource_block)
+        else:
+            global_logger.info(f"No such resource_type: {resource_type}")
+            return None
     
-    # @abstractmethod
-    # def get_supported_resources(self) -> List[str]:
-    #     """
-    #     Returns a list of supported resource types for the provider.
-    #     Returns:
-    #         List[str]: Supported resource types (e.g., ["aws_instance", "aws_s3_bucket"]).
-    #     """
-    #     pass
